2933f3a5bff1ece0114461bf1ae5804f03dc0674
paddy123456789 <69131508+paddy123456789@users.noreply.github.com>
2020-08-06 16:58:52 +0800

    Add files via upload

---
+++ star.py
@@ -0,0 +1,5 @@
+a = int(input())
+total = 1
+for i in range(1,a+1):
+    total = total * i
+print(total)
---
+++ test2.py
@@ -0,0 +1,3 @@
+i = int(input('請問要幾層'))
+for e in range(1,i + 1):
+    print(' '*(i-e),'*'*e)
